c32f373a3b9ad9b93bc1704aa8430f6568319631
Michal Jaworski 2020 <borzoj@gmail.com>
2020-10-16 21:39:11 +0100

    s3 download, dynamodb put, dynamodb read

--- handler-one.js
+++ handler-one.js
@@ -1,20 +1,49 @@
 'use strict';
 
-module.exports.main = async event => {
-  console.log('here one')
-  console.log(event)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'step one',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require('aws-sdk')
+const crypto = require('crypto')
+const s3 = new AWS.S3()
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.main = async event => {
+  try {
+    console.log('here one')
+    console.log(event)
+    const key = event.key;
+    const bucket = process.env.bucket;
+    const table = process.env.table;
+    const id = crypto.randomBytes(16).toString('hex')
+    const blob = await s3
+      .getObject({ Bucket: bucket, Key: key })
+      .promise();
+    console.log("got s3 result");
+    var params = {
+      TableName:table,
+      Item:{
+          "id": id,
+          "hl7": blob.Body.toString('utf-8'),
+      }
+    }
+    console.log("sending to dynamodb")
+    const result = await docClient.put(params).promise()
+    console.log("sent")
+    console.log(result)
+    return {
+      statusCode: 200,
+      body: {id},
+    }
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: 'step one',
+          input: event,
+        },
+        null,
+        2
+      ),
+    }
+  }
+}
--- handler-two.js
+++ handler-two.js
@@ -1,20 +1,40 @@
 'use strict';
 
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
+
 module.exports.main = async event => {
+  try {
     console.log('here two')
     console.log(event)
+    const id = event.body.id
+    console.log(id)
+    const table = process.env.table;
+    var params = {
+      TableName: table,
+      Key: {id}, 
+    };
+    console.log('getting item from dynamo')
+    console.log(params)
+    const result = await docClient.get(params).promise()
+    console.log('got result')
+    console.log(result)
     return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'step two',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+      statusCode: 200,
+      body: {},
+    }
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: 'step one',
+          input: event,
+        },
+        null,
+        2
+      ),
+    }
+  }
+}
